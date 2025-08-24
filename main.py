85210e838762bee5a7edd1f79130a98af39b82c9
Volcanoscript <142372718+Volcanoscript@users.noreply.github.com>
2025-08-25 07:17:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ app = Flask(__name__)
 WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
 
 # Your default message (you can edit anytime)
-MESSAGE = "@everyone Join now! New Roblox Condo Updated [https*:*//www.roblox.com/games/17427610333/neko-rng-ALPHA?privateServerLinkCode=79949483018803329508950212279688](https://rbx-url.com/vP7B-1Zf)"
+MESSAGE = "@everyone New Roblox Condo Released Workimg Now [https*:*//www.roblox.com/games/17427610333/neko-rng-ALPHA?privateServerLinkCode=79949483018803329508950212279688](https://rbx-url.com/vP7B-1Zf)"
+
 # Interval in seconds (example: 600 = 10 minutes, 60 = 1 minute)
 INTERVAL_SECONDS = 600  
 
